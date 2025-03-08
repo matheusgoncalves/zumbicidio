d2e9206a9a005fc2206124ce46d4d028133e2f8b
Matheus <matheus.goncalves@gamese.gg>
2025-03-08 18:38:46 -0300

    feat(zumbi): implementação da classe zumbi e suas variáveis. Closes #8

---
+++ Zumbi.java
@@ -0,0 +1,29 @@
+public abstract class Zumbi extends Personagem {
+    public Zumbi(int saude, int x, int y) {
+        super(saude, x, y);
+    }
+
+    public abstract void mover(int x, int y);
+
+    public  boolean validarAtaque(Arma arma) {
+        return true;
+    }
+
+    public void atacar(Jogador jogador) {
+        jogador.receberDano(1);
+    }
+
+    public void receberDano(int dano) {
+        saude -= dano;
+        System.out.println("Zumbi sofreu " + dano + " de dano!");
+
+        if (saude <= 0) {
+            System.out.println("Zumbi eliminado!");
+            // Remover zumbi da tela
+        }
+    }
+
+    public String getMensagemErro() {
+        return "Ataque inválido!";
+    }
+}
