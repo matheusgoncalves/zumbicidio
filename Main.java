224e79acd3f4ec0eb167b32465915773dc93a1a3
rdscaetano <rdscaetano@inf.ufpel.edu.br>
2025-03-12 12:18:00 -0300

    atualização no movimento e verificações na matriz que impedira de sumir com baus, mais sprites e bau aberto após passar por cima

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         Mapa mapa = new Mapa(Mapa.MAPA1, 0, 0);
+        Jogador jogador = new Jogador(0,0,0);
+
 
         JFrame janela = new JFrame("Mapa Grid");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -13,6 +15,18 @@ public class Main {
         InterfaceMapa painelMapa = new InterfaceMapa(mapa);
         janela.add(painelMapa, BorderLayout.CENTER);
 
+
+        JFrame combate = new JFrame("Combate");
+        combate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        combate.setSize(200, 600);
+
+        combate.setLocation(600,0);
+        InterfaceCombate painelCombate = new InterfaceCombate(jogador);
+
+
+
+
         janela.setVisible(true);
+        combate.setVisible(true);
     }
 }
